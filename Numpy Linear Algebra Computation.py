1336a49786016dd2c375e65e53527819a372a0f2
Will-Harris00 <wjharris00@outlook.com>
2020-06-12 22:40:31 +0100

    Code Refactoring

--- Numpy Linear Algebra Computation.py
+++ Numpy Linear Algebra Computation.py
@@ -1,101 +1,44 @@
 import numpy as np
 import pandas as pd
-import timeit as t
+import timeit as tm
 import matplotlib.pyplot as plt
 
 
-def matrix5():
-    nfive_matrix = np.random.rand(5, 5)
-    ifive_matrix = np.linalg.inv(nfive_matrix)
-    return ifive_matrix
-
-def matrix10():
-    nten_matrix = np.random.rand(10, 10)
-    iten_matrix = np.linalg.inv(nten_matrix)
-    return iten_matrix
-
-def matrix20():
-    ntwenty_matrix = np.random.rand(20, 20)
-    itwenty_matrix = np.linalg.inv(ntwenty_matrix)
-    return itwenty_matrix
-
-def matrix50():
-    nfifty_matrix = np.random.rand(50, 50)
-    ififty_matrix = np.linalg.inv(nfifty_matrix)
-    return ififty_matrix
-
-def time():
-    iterations = 9999
-    timing5 = []
-    timing10 = []
-    timing20 = []
-    timing50 = []
-    for i in range (5):
-        time5 = t.timeit(matrix5, number=iterations)
-        print("\n 5x5 matrix run " + str(i+1) + " with " + str(iterations) +
-              " iterations completed in a time of "+ str(time5))
-        timing5.append(time5)
-
-        time10 = t.timeit(matrix10, number=iterations)
-        print("\n 10x10 matrix run " + str(i+1) + " with " + str(iterations) +
-              " iterations completed in a time of "+ str(time10))
-        timing10.append(time10)
-
-        time20 = t.timeit(matrix20, number=iterations)
-        print("\n 20x20 matrix run " + str(i+1) + " with " + str(iterations) +
-              " iterations completed in a time of "+ str(time20))
-        timing20.append(time20)
-
-        time50 = t.timeit(matrix50, number=iterations)
-        print("\n 50x50 matrix run " + str(i+1) + " with " + str(iterations) +
-              " iterations completed in a time of "+ str(time50))
-        timing50.append(time50)
-        print("\n")
-    print(str(timing5) + "\n" + str(timing10) + "\n" + str(timing20) + "\n" + str(timing50) + "\n")
-    return timing5, timing10, timing20, timing50
-
-def cal_average(timing5, timing10, timing20, timing50):
-    sum_num = 0
-    for t in timing5:
-        sum_num = sum_num + t
-
-    avg5 = sum_num / len(timing5)
-    print("Average time to complete one run of 9999 iterations "
-          "for a random 5x5 matrix " +str(avg5))
-
-    sum_num = 0
-    for t in timing10:
-        sum_num = sum_num + t
-
-    avg10 = sum_num / len(timing10)
-    print("Average time to complete one run of 9999 iterations "
-          "for a random 10x10 matrix " +str(avg10))
-
-    sum_num = 0
-    for t in timing20:
-        sum_num = sum_num + t
-
-    avg20 = sum_num / len(timing20)
-    print("Average time to complete one run of 9999 iterations "
-          "for a random 20x20 matrix " +str(avg20))
-
-    sum_num = 0
-    for t in timing50:
-        sum_num = sum_num + t
-
-    avg50 = sum_num / len(timing50)
-    print("Average time to complete one run of 9999 iterations "
-          "for a random 50x50 matrix " +str(avg50))
-
-    return avg5, avg10, avg20, avg50
-
-
-def curve(avg):
+def main(nmax, iterations, repeats):
+    global j
     time_dict = {}
-    time_dict.update({5 : [5, avg[0]]})
-    time_dict.update({10 : [10, avg[1]]})
-    time_dict.update({20 : [20, avg[2]]})
-    time_dict.update({50 : [50, avg[3]]})
+    for n in range (1, nmax+1):
+        j = n
+        times_list = []
+        for i in range (0, repeats):
+            time = tm.timeit(matrix, number=iterations)
+            print("\n " + str(n) + "x" +str(n) +" matrix run " + str(i + 1) +
+                  " with " + str(iterations) +
+                  " iterations completed in a time of " + str(time))
+            times_list.append(time)
+
+        sum = 0
+        for t in times_list:
+            sum = sum + t
+
+        avg = sum / len(times_list)
+        print("\nAverage time to complete one run of " +
+              str(iterations) + " iterations for a random " + str(n) +
+              "x" + str(n) + " matrix = " + str(avg) + "\n\n")
+
+        time_dict.update({n: [n, avg]})
+    print(time_dict)
+    dataFrame(time_dict)
+
+
+def matrix():
+    global j
+    nmatrix = np.random.rand(j, j)
+    imatrix = np.linalg.inv(nmatrix)
+    return imatrix
+
+
+def dataFrame(time_dict):
     df = pd.DataFrame.from_dict(time_dict, orient='index', columns=['Size', 'Average Time'])
     print(df)
     df.plot(x='Size', y='Average Time', kind='line'
@@ -106,6 +49,8 @@ def curve(avg):
     plt.show()
 
 if __name__ == '__main__':
-    times = time()
-    avg = cal_average(times[0], times[1], times[2], times[3])
-    curve(avg)
+    j = 0
+    main(20, 9999, 5)
+    #times = time()
+    #avg = cal_average(times[0], times[1], times[2], times[3])
+    #curve(avg)
